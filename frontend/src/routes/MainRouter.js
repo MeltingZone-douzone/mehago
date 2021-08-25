e2c5f5f7a2c0eaf0b9213ad2c771cde6b7d2cde5
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-08-25 16:00:07 +0900

    merge

--- frontend/src/routes/MainRouter.js
+++ frontend/src/routes/MainRouter.js
@@ -4,6 +4,7 @@ import { Route, Switch, BrowserRouter as Router, Redirect } from 'react-router-d
 
 import ProfileSettingsPage from './pages/ProfileSettingsPage';
 import AccountPage from './pages/AccountPage';
+import SettingChatRoom from '../chat/SettingChatRoom';
 import Header from '../components/Header';
 
 
@@ -16,11 +17,10 @@ export default function MainRouter() {
                 <Header />
                 <WebPage>
                     <Switch>
-                        <Route exact path="/"><Redirect to="/account/login"/></Route>
-                        
-                        <Route path="/profile"  component={ProfileSettingsPage}/>
-                        <Route path="/account" component={AccountPage}/>
-
+                        <Route exact path="/"><Redirect to="/account/login" /></Route>
+                        <Route path="/profile" component={ProfileSettingsPage} />
+                        <Route path="/account" component={AccountPage} />
+                        <Route path="/room" component={SettingChatRoom} />
                     </Switch>
                 </WebPage>
             </Fragment>
